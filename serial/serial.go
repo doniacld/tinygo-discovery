01a217e21989378cae9707fe99b1632c4efff6de
Donia Chaiehloudj <donia.cld@gmail.com>
2022-03-14 20:46:19 +0100

    update serial with hour in timestamp

--- serial/serial.go
+++ serial/serial.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	for {
 		now := time.Now()
-		fmt.Printf("%02d:%02d, Hello Women Tech Makers!\n", now.Minute(), now.Second())
+		fmt.Printf("%02d:%02d:%02d, Hello Women Tech Makers!\n", now.Hour(), now.Minute(), now.Second())
 
 		time.Sleep(time.Second)
 	}
