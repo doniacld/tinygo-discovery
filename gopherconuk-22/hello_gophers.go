6749cb66e3049136810eece3bdd5aeb9c9df7bfa
Donia Chaiehloudj <donia.cld@gmail.com>
2022-08-14 18:49:09 +0200

    Add gophercon examples

---
+++ gopherconuk-22/hello_gophers.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+	"time"
+)
+
+func main() {
+	for {
+		log.Println("Hi London! What's up GopherCon UK 2022?")
+		time.Sleep(time.Second)
+	}
+}
---
+++ gopherconus-22/hello_gophers.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+	"time"
+)
+
+func main() {
+	for {
+		log.Println("Hi Chicago! What's up Gophers?")
+		time.Sleep(time.Second)
+	}
+}
